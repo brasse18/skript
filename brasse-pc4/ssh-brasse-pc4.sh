f6b05663c2e717015138e1059ecf85bb93400fcf
brasse <brasse.bb@gmail.com>
2017-04-12 16:35:32 +0200

    edit skript fixt uppdate of arp tabel

--- brasse-pc4/ssh-brasse-pc4.sh
+++ brasse-pc4/ssh-brasse-pc4.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+BROD="$(ip address show $DEV | grep 'inet .* brd ' | head -1 | sed -e 's/^.* brd \([0-9\.]*\) .*$/\1/')"
+
+ping -c 1 -b "${BROD}"
+
 IP="$(arp -a | grep 08:60:6e:15:2b:f3 | sed 's/^.*(/(/' | grep -Eo '.*?[)]' | sed 's/[()]//g')"
 ssh "brasse@${IP}"
 
